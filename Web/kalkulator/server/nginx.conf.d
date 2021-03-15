d5ab032330506196dde0f028f0bfde535a205c34
cacadosman <wetmanz23@gmail.com>
2021-03-15 21:45:51 +0700

    fix docker compose

--- Web/kalkulator/server/nginx.conf.d
+++ Web/kalkulator/server/nginx.conf.d
@@ -7,7 +7,7 @@ server {
     error_log  /var/log/nginx/error.log;
     access_log /var/log/nginx/access.log;
     location / {
-        proxy_pass http://kalkulator;
+        proxy_pass http://kalkulator-app;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $remote_addr;
     }
